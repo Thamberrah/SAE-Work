24575e21b530413f89a0a430b51e7b6bf6aa832f
Thamber Rahman <thamber02@hotmail.com>
2022-01-19 16:36:49 +0000

    Experimentation-files

---
+++ term-2/index.js
@@ -0,0 +1,101 @@
+
+/*    document.write("Goodbye life, I am now dead");
+
+    document.getElementById("text").innerHTML="This is different text"; // Displays a different set of data, as the 'innerHTML' element replaces the original data that is represented by the 'ID'.
+
+    var x=100;
+    document.write(x);
+
+    var x=300;
+    document.write(x);
+
+    let x=100;
+    document.write(x);
+
+    let x=200;
+    document.write(x);
+
+*/
+/*
+    var num = 10;
+    document.write(typeof(num), "\n"); //typeof returns the type of variable that is called.
+    num = '200';
+    document.write(typeof(num), "\n");
+    
+    document.write(num);
+
+*/
+
+/*
+    let x=99;
+    document.write("the value of x is", x, "\t");
+
+        {
+            let x = 50;
+            document.write(x), "\n";
+        }
+        
+        {
+            let x = 200;
+            document.write(x);
+            document.write("the value of x",+ x);
+        }
+
+*/
+
+/*
+    window.alert("This is an alert!")
+*/
+
+/*
+    console.log("Why have you done this to me?")
+*/
+
+/*
+    var carName;
+    carName = "Toyota";
+    document.write(carName);
+    console.log(carName)
+    
+*/
+
+/*
+    var x = 5;
+    var y = 5;
+
+    x ++; //Increments the value by 1
+    document.write(x, "\n");
+    
+    y --; //Decrements the value by 1
+    document.write(y, "\n");
+
+
+
+    var c = 10;
+
+    c+=2;
+    // This is a concise version of 'c= c+5'
+    document.write(c, "\n");
+    console.log(c)
+
+    var d = 12;
+
+    d-=2;
+    // This is a concise version of 'd= d-2'
+    document.write(d, "\n");
+    console.log(d)
+
+    var e = 15;
+    // This is a concise version of 'e= e*2'
+    e*=2;
+    document.write(e, "\n");
+    console.log(e);
+
+    var f = 20;
+    //This is a concise version of 'f= f/4'
+    f/=4;
+    document.write(f);
+    console.log(f);
+*/
+
+document.getElementById('text').style.backgroundColor = 'red';
