812ab7062daf8bc788afb28af2717b8155a17617
Thamberrah <thamberoot@hotmail.com>
2022-01-26 16:53:51 +0000

    commits made

---
+++ term-2/commentOut/js/index.js
@@ -0,0 +1,53 @@
+/*let text = 'We are the "Vikings" from the East.'
+
+let textTwo = "We are the \"Vikings\" from the North." //Turn quotes into special characters
+
+var textThree = 'Its \"Monday\" '
+
+console.log(text);
+console.log(textTwo);*/
+
+/*var a = 10;
+var b = 10;
+var s = (a==b)
+console.log(s);
+document.write(s);*/ // Shows as a boolean, based on the operation on line 12
+
+/*var student = {
+    fName: "lyra",
+    lName: "Smith",
+    age : "24",
+};
+document.write(student.fName)*/
+
+/*var pet = {
+    pName: "Cooper",
+    pType: "Dog"
+};
+
+document.write(pet.pName)
+pet.color='dead'
+document.write(pet.color) // cooperdead is the output
+*/
+
+var employee = {
+    fName: "Damien",
+    lName: "Smithereens",
+    age: 95,
+    salary: 20,
+    address: "Not Needed"
+}
+
+document.write(employee.fName, "\n")
+document.write(employee.lName, "\n")
+document.write(employee.age, "\n")
+document.write(employee.salary, "\t")
+document.write(employee.address, "\n")
+
+
+/*var num=[];
+num[0] = 20;
+num[1] = 200;
+document.write(num);
+*/
+
